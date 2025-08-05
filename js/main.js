ce53a9201318b44b98e8245ce09cc09d726ea62a
aya <ayaomarhagres@gmail.com>
2025-08-05 16:16:36 +0300

    test

--- js/main.js
+++ js/main.js
@@ -6,14 +6,16 @@ $(function(){
 
     // featured product
 
-    $(".services--slider").owlCarousel({
+    $(".managent").owlCarousel({
+        stagePadding: 200,
+        margin:10,
         autoplay: true,
         autoplayhoverpause: true,
         autoplaytimeout: 100,
-        items: 4,
+        items: 3,
         nav: false,
         loop: true,
-        dots: true,
+        dots: false,
         responsive: {
             0 : {
                 items: 1
@@ -25,37 +27,9 @@ $(function(){
                 items: 3
             },
             1200 : {
-                items: 4
+                items: 3
             }
         }
     });
-
-    $('.main-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.small-slider'
-      });
-      $('.small-slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.main-slider',
-        dots: false,
-        centerMode: true,
-        focusOnSelect: true
-      });
-    
-
-    $('.btn-faq').click(function(){
-        $(this).toggleClass('active');
-    });
-
-
-    $('.close-shop-cart').click(function (){
-
-        $($(this).data('close')).fadeOut();
-    });
-    
 
 });
